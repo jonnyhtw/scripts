68492d6cc6f5fc0cd74964eebc4d6c6f227fa0bc
Jonny Williams <jonny.williams@niwa.co.nz>
2018-11-08 05:16:00 +0000

    lots of py and ipynb changes

--- climatemeans.py
+++ climatemeans.py
@@ -1,6 +1,3 @@
-
-
-
 import cartopy.crs as ccrs
 import matplotlib
 import matplotlib.pyplot as plt
@@ -20,10 +17,6 @@ import cartopy.crs as ccrs
 import iris.analysis.cartography
 import re
 from os import listdir
-
-
-
-
 
 in_dir_base='/home/williamsjh/cylc-run/'
 runid='bc048'
@@ -46,40 +39,34 @@ nyears=20
 if not os.path.exists(out_dir+'/supermeans'):
     os.makedirs(out_dir+'/supermeans')
 
-if len(years) == 2:
+if nyears == 2:
     supermeanlabel = '2'
 
-if len(years) == 10:
+if nyears == 10:
     supermeanlabel = 'a'
     
-if len(years) == 20:
+if nyears == 20:
     supermeanlabel = 'k'
 
-if len(years) == 2:
+if nyears == 2:
     supermeanlabel = '2'
 
-if len(years) >= 30 and len(years) <40:
+if nyears >= 30 and nyears <40:
     supermeanlabel = 't'
 
-if len(years) >= 50 and len(years) <100:
+if nyears >= 50 and nyears <100:
     supermeanlabel = 'l'
 
-fnames = list(braceexpand(in_dir+'*pm*196[1-2]*{mar,apr,may}*'))
+fnames = list(braceexpand(in_dir+'*pm*{196[0-9],197[0-9]}*{mar,apr,may}*'))
 
-months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+period = 'mam'
 
-for year in range(firstyear,firstyear+nyears):
+print fnames
 
-    for month in months:
-        
-        print str(year)+':'+str(month)
+vars = iris.load(fnames)
 
-        file =  'bc048a.pm'+str(year)+str(month)+'.pp'
-         
-        print file
+#varsmean = vars.collapsed('time',iris.analysis.MEAN)
 
-        vars = iris.load(in_dir+'/'+file)
-
-        iris.save(vars,out_dir+'/supermeans'+'/'+runid+'a.m'+supermeanlabel+str(years[-1])+month+'.nc')
+#iris.save(varsmean,out_dir+'/supermeans'+'/'+runid+'a.m'+supermeanlabel+str(year)+period+'.pp')
         # the m in the previous line is the 'mean' indicator, not to be
         # confused with the 'm for monthly' in the input files!
