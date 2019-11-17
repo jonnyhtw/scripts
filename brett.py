c9f5ac5f4059db425783f2bcc6ca4d4f7fd98954
Jonny Williams <jonny.williams@niwa.co.nz>
2019-11-17 23:46:36 +0000

    improving script for getting brett's data.

--- brett.py
+++ brett.py
@@ -1,35 +1,26 @@
-import iris
-import numpy as np
-import glob, os
+import iris, glob
 
 suite = 'u-bl658'
 
 stash = ['m01s16i222','m01s05i216','m01s00i024']
 
-hourly_constraint = iris.Constraint(cube_func=lambda cube: cube.cell_methods[0].intervals[0]=="1 hour")
+for year in range(1950,2015):
 
-for i in range(len(stash)):
+    print(year)
 
-    print(i)
-    print(stash[i])
+    for i in range(len(stash)):
 
-    stash_constraint = iris.AttributeConstraint(STASH=stash[i])
+        stash_constraint = iris.AttributeConstraint(STASH=stash[i])
 
-    files = sorted(glob.glob('/home/williamsjh/cylc-run/'+suite+'/share/data/History_Data/*a.pm*1950*.pp'))
-    print(files)
+        files = sorted(glob.glob('/home/williamsjh/cylc-run/'+suite+'/share/data/History_Data/*a.pm*'+str(year)+'*.pp'))
+        print(files)
 
-    #if stash[i] == 'm01s05i216' or stash[i] == 'm01s00i024':
-    #    print('i am in the if statement')
-    #    cubes = iris.load(files, stash_constraint & hourly_constraint)
-    #else:
-    #    print('i am in the else statement')
-    cubes = iris.load(files, stash_constraint)
+        cubes = iris.load(files, stash_constraint)
 
-    for cube in cubes:
-        print(cube.cell_methods[0].intervals[0])
-        if cube.cell_methods[0].intervals[0] == '1 hour' or cube.cell_methods[0].intervals[0] == '6 hour':
-            iris.save(cube, '/nesi/project/niwa00013/williamsjh/NZESM/brett-validation-metrics/'+suite+'-'+stash[i]+'-monthly.nc')
+        for cube in cubes:
+            print(cube.cell_methods[0].intervals[0])
+            if cube.cell_methods[0].intervals[0] == '1 hour' or cube.cell_methods[0].intervals[0] == '6 hour':
+                iris.save(cube, '/nesi/project/niwa00013/williamsjh/NZESM/brett-validation-metrics/'+suite+'-'+stash[i]+'-'+str(year)+'-monthly.nc')
 
-    del cubes, cube
-
+        del cubes, cube
 
