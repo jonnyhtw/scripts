d30b44780a4b0c69ce13dd91dc597bbca1c06fac
Jonny Williams <jonny.williams@niwa.co.nz>
2019-12-11 22:13:25 +0000

    generalising cr

--- compress_for_nearline.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd /nesi/nobackup/niwa00013/williamsjh/cylc-run/u-bl274/share/data/History_Data
-
-for year in {1962..1963} ; do echo $year ; tar -cvzf bl274a.${year}.tar.gz --remove-files *${year}* ; done
-
---
+++ howmanyfilesinhere.sh
@@ -0,0 +1,7 @@
+echo 'there are this many files in...'
+echo $PWD
+find . -true -print | wc -l
+
+
+echo 'and this is how they are distributed across folders (including hidden ones)...'
+find . -maxdepth 1 -mindepth 1 -type d -print0 | xargs -0 -I {} sh -c 'echo -n "{}      " && { find {} -true -print | wc -l ; }'
