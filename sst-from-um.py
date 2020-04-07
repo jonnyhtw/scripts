abbde25739f19453d7a44345f46d994730c85aaf
Jonny Williams <jonny.williams@niwa.co.nz>
2020-04-07 10:24:22 +0000

    misc improvements

--- sst-from-um.py
+++ sst-from-um.py
@@ -10,16 +10,15 @@ mask_constraint = iris.AttributeConstraint(STASH='m01s00i030')
 desired_cell_method = iris.coords.CellMethod(method='mean', coords='time', intervals='1 hour')
 cell_method_constraint = iris.Constraint(cube_func=lambda cube: desired_cell_method in cube.cell_methods)
 
-files = np.sort(glob.glob('/nesi/nobackup/niwa00013/williamsjh/cylc-run/u-bl658/share/data/History_Data/*a.pm*.pp'))
+mask = iris.load_cube('/nesi/nobackup/niwa00013/williamsjh/cylc-run/u-bl658/share/data/History_Data/bl658a.pm1950jan.pp',mask_constraint)
 
+for year in range(1950, 2015):
 
+    files = np.sort(glob.glob('/nesi/nobackup/niwa00013/williamsjh/cylc-run/u-bl658/share/data/History_Data/*a.pm'+str(year)+'*.pp'))
 
-mask = iris.load('/nesi/nobackup/niwa00013/williamsjh/cylc-run/u-bl658/share/data/History_Data/bl658a.pm1950jan.pp',mask_constraint)
+    cubes = iris.load_cube(files, stash_constraint & cell_method_constraint)
 
+    for i in range(0,cubes.data.shape[0]):
+        cubes.data[i,:,:][mask.data == 1] = np.nan
 
-for file in files:
-    print('now doing... ',file)
-    cubes = iris.load(file, stash_constraint & cell_method_constraint)
-    cubes[0].data = ma.array(cubes[0].data, mask=mask[0].data)
-    iris.save(cubes,'./'+os.path.basename(file)[:-3]+'-masked-monthly-sst.nc')
-
+    iris.save(cubes,'bl658-'+str(year)+'-masked-sst.nc')
