5f25c034fe3983f2f7293c8758a01a57a9c80825
Jonny Williams <jonny.williams@niwa.co.nz>
2020-10-01 23:57:49 +0000

    remove whitespace

--- area_avg_cube.py
+++ area_avg_cube.py
@@ -6,7 +6,7 @@ def area_avg_cube(cube, guess_bounds=False, missing_data_value=None, stdev=False
     if missing_data_value is not None: 
         my_masked_array = np.ma.masked_values(cube.data, missing_data_value, copy=False)
         cube.data = my_masked_array
-    
+
     grid_areas = iris.analysis.cartography.area_weights(cube)
     
     if stdev:
