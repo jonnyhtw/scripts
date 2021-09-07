d2620d50fe244e1f08a9472e36bfa888e57de6e2
Jonny Williams <jonny.williams@niwa.co.nz>
2021-09-07 00:12:28 +0000

    now use argparse

--- quick-access-nzesm-data.py
+++ quick-access-nzesm-data.py
@@ -5,7 +5,6 @@ import subprocess
 stashmodel = '01'
 rawstashs=np.array(['34001','03236','30207','30201','30202','00409','30204']) 
 descriptions = np.array(['O3_MMR','Temperature_at_1.5m','Geopotential_height_on_P_levs','U_wind_on_P_levs','V_wind_on_P_levs','Surface_pressure','Temperature_on_P_levs'])
-descriptions = np.array(['U_wind_on_P_levs','V_wind_on_P_levs'])
 suite = 'u-bn013'
 years = range(2015,2100)
 months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
@@ -18,9 +17,20 @@ for k in range(len(years)):
 
             indir = '/nesi/nobackup/niwa00013/williamsjh/nearline/niwa00013/williamsjh/cylc-run/'+suite+'/share/data/History_Data/'
 
+            infile = suite[2:]+'a.pm'+str(years[k])+months[i]+'.pp'
+
             outdir ='/nesi/project/niwa00013/williamsjh/NZESM/quick-access-data/u-bn013/' 
 
-            mulecom = 'mule-select '+indir+suite[2:]+'a.pm'+str(years[k])+months[i]+'.pp'+outdir+' m'+stashmodel+'s'+rawstashs[j][0:2]+'i'+rawstashs[j][2:]+'-'+descriptions[j]+'-'+suite[2:]+'a.pm'+str(years[k])+months[i]+'.pp --include lbuser4='+rawstashs[j]
+            outfile = 'm'+stashmodel+'s'+rawstashs[j][0:2]+'i'+rawstashs[j][2:]+'-'+descriptions[j]+'-'+suite[2:]+'a.pm'+str(years[k])+months[i]+'.pp'
+
+            mulecom = 'mule-select '+\
+                      indir+\
+                      infile+\
+                      ' '+\
+                      outdir+\
+                      outfile+\
+                      ' '+\
+                      '--include lbuser4='+rawstashs[j]
 
             os.system('module load Mule;'+  mulecom)
 
