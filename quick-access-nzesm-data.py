2bb1f35649c9ff7a3125feb7c83684cdee9286a8
Jonny Williams <jonny.williams@niwa.co.nz>
2021-09-07 06:50:35 +0000

    new sl, new alias, new arg

--- quick-access-nzesm-data.py
+++ quick-access-nzesm-data.py
@@ -8,13 +8,17 @@ import argparse
 stashmodel = '01'
 rawstashs=np.array(['34001','03236','30207','30201','30202','00409','30204','00024','03234','01208','02205','34049','34992','34010','34081','00010','00408']) 
 descriptions = np.array(['O3_MMR','Temperature_at_1.5m','Geopotential_height_on_P_levs','U_wind_on_P_levs','V_wind_on_P_levs','Surface_pressure','Temperature_on_P_levs','Surface_temperature','Surface_latent_heat_flux','TOA_outgoing_SW','TOA_outgoing_LW','N2O_MMR','HCl_MMR','CO_MMR','OH_MMR','Specific_humidity','Pressure_on_theta_levs'])
-suite = 'u-bn013'
 months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 
+#####################################################
+#parse some args
 parser = argparse.ArgumentParser()
 parser.add_argument('--year', required=True, type=int)
+parser.add_argument('--suite', required=True, type=str)
 args = parser.parse_args()
 year = args.year
+suite = args.suite
+#####################################################
 
 for j in range(len(rawstashs)):
 
@@ -27,6 +31,9 @@ for j in range(len(rawstashs)):
         outdir ='/nesi/project/niwa00013/williamsjh/NZESM/quick-access-nzesm-data/'+suite+'/' 
 
         outfile = 'm'+stashmodel+'s'+rawstashs[j][0:2]+'i'+rawstashs[j][2:]+'-'+descriptions[j]+'-'+suite[2:]+'a.pm'+str(year)+months[i]+'.pp'
+
+        #create outdir if it's not there
+        os.system('mkdir -p '+outdir)
 
         #define mule command
         mulecom = 'mule-select '+\
---
+++ quick-access-nzesm-data.sl
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+# DIRECTIVES:
+#SBATCH --time=24:00:00
+#SBATCH --job-name=quick-access-nzesm-data
+#SBATCH --partition=nesi_prepost
+#SBATCH --cpus-per-task=1
+#SBATCH --account=niwa00013
+#SBATCH --hint=nomultithread
+##SBATCH --mem-per-cpu=200G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --array=2015-2100
+
+module load Mule
+
+/opt/nesi/CS500_centos7_skl/Anaconda2/2019.10-GCC-7.1.0/bin/python -u quick-access-nzesm-data.py --year=$SLURM_ARRAY_TASK_ID --suite=u-bp908
+
