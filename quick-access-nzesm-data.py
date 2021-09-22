2516cfcfac4af0b8d2785ecd30e485647729b23c
Jonny Williams <jonny.williams@niwa.co.nz>
2021-09-22 01:38:20 +0000

    fixing git commit error in vi/vim finally... and adding more vars into nzesm quick access stream

--- quick-access-nzesm-data.py
+++ quick-access-nzesm-data.py
@@ -6,8 +6,17 @@ import subprocess
 import argparse
 
 stashmodel = '01'
-rawstashs=np.array(['34001','03236','30207','30201','30202','00409','30204','00024','03234','01208','02205','34049','34992','34010','34081','00010','00408']) 
-descriptions = np.array(['O3_MMR','Temperature_at_1.5m','Geopotential_height_on_P_levs','U_wind_on_P_levs','V_wind_on_P_levs','Surface_pressure','Temperature_on_P_levs','Surface_temperature','Surface_latent_heat_flux','TOA_outgoing_SW','TOA_outgoing_LW','N2O_MMR','HCl_MMR','CO_MMR','OH_MMR','Specific_humidity','Pressure_on_theta_levs'])
+rawstashs=np.array(['34001','03236','30207','30201','30202','00409','30204','00024','03234','01208','02205','34049','34992','34010','34081','00010','00408','50219','50245','00002','00003','50228','50229']) 
+
+descriptions = np.array(['O3_MMR','Temperature_at_1.5m','Geopotential_height_on_P_levs','U_wind_on_P_levs','V_wind_on_P_levs','Surface_pressure','Temperature_on_P_levs','Surface_temperature','Surface_latent_heat_flux','TOA_outgoing_SW','TOA_outgoing_LW','N2O_MMR','HCl_MMR','CO_MMR','OH_MMR','Specific_humidity','Pressure_on_theta_levs','Ozone_column_in_DU','Photolysis_rate_JO2','U_wind_on_model_levels','V_wind_on_model_levels','Photolysis_rate_JO1D','Photolysis_rate_JNO2'])
+
+streams = np.array(['m','m','m','m','m','m','m','m','m','m','m','m','m','m','m','m','m','m','m'])
+
+if ( len(rawstashs) == len(descriptions) != len(streams) ):
+    print('rawstashs descriptions and streams must be the same length')
+    exit()
+    
+
 months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 
 #####################################################
@@ -26,11 +35,11 @@ for j in range(len(rawstashs)):
 
         indir = '/nesi/nobackup/niwa00013/williamsjh/nearline/niwa00013/williamsjh/cylc-run/'+suite+'/share/data/History_Data/'
 
-        infile = suite[2:]+'a.pm'+str(year)+months[i]+'.pp'
+        infile = suite[2:]+'a.p'+streams[i]+str(year)+months[i]+'.pp'
 
         outdir ='/nesi/project/niwa00013/williamsjh/NZESM/quick-access-nzesm-data/'+suite+'/' 
 
-        outfile = 'm'+stashmodel+'s'+rawstashs[j][0:2]+'i'+rawstashs[j][2:]+'-'+descriptions[j]+'-'+suite[2:]+'a.pm'+str(year)+months[i]+'.pp'
+        outfile = 'm'+stashmodel+'s'+rawstashs[j][0:2]+'i'+rawstashs[j][2:]+'-'+descriptions[j]+'-'+suite[2:]+'a.p'+streams[i]+str(year)+months[i]+'.pp'
 
         #create outdir if it's not there
         os.system('mkdir -p '+outdir)
