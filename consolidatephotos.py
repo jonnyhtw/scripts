363d91f67a1b40df48ee3e4a9a0ba18435755120
Jonny Williams <jonny.williams@niwa.co.nz>
2023-07-23 22:46:06 +1200

    black

--- consolidatephotos.py
+++ consolidatephotos.py
@@ -8,21 +8,18 @@ import shutil
 for f in tqdm(os.listdir(".")):
 
     if os.path.isfile(f):
-        r = f.replace(" ","_")
-        if( r != f):
-            os.rename(f,r)
+        r = f.replace(" ", "_")
+        if r != f:
+            os.rename(f, r)
 
 for f in tqdm(os.listdir(".")):
 
     date = datetime.now().isoformat()
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    
+
     if os.path.isfile(f):
-        os.rename(f,str(date)+'-'+f)
+        os.rename(f, str(date) + "-" + f)
 
 for f in tqdm(os.listdir(".")):
     if os.path.isfile(f):
-        shutil.move(f,'/Volumes/Data/Pictures/')
-
-
-
+        shutil.move(f, "/Volumes/Data/Pictures/")
