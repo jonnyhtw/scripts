98514588360a960390babf9fa680f3747e7f4c96
Jonny Williams <jonny.williams@niwa.co.nz>
2023-07-23 22:44:46 +1200

    check that it's a file

--- consolidatephotos.py
+++ consolidatephotos.py
@@ -5,19 +5,24 @@ from tqdm import tqdm
 from datetime import datetime
 import shutil
 
-
-
 for f in tqdm(os.listdir(".")):
 
-    r = f.replace(" ","_")
-    if( r != f):
-        os.rename(f,r)
+    if os.path.isfile(f):
+        r = f.replace(" ","_")
+        if( r != f):
+            os.rename(f,r)
 
 for f in tqdm(os.listdir(".")):
 
     date = datetime.now().isoformat()
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    os.rename(f,str(date)+'-'+f)
+    
+    if os.path.isfile(f):
+        os.rename(f,str(date)+'-'+f)
 
 for f in tqdm(os.listdir(".")):
-    shutil.move(f,'/Volumes/Data/Pictures/')
+    if os.path.isfile(f):
+        shutil.move(f,'/Volumes/Data/Pictures/')
+
+
+
