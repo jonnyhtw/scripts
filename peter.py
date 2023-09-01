648f733a81b2a8e411151713ca0ffd16452c8bb5
Jonny Williams <jonny.williams@niwa.co.nz>
2023-09-01 16:40:07 +1200

    script for peter

---
+++ peter.py
@@ -0,0 +1,15 @@
+#!/nesi/nobackup/niwa00013/williamsjh/miniconda3/envs/metpy/bin/python
+
+import iris
+import iris.coords
+import os
+import glob
+from tqdm import tqdm
+
+stash_constraint  =iris.AttributeConstraint(STASH='m01s03i236')
+desired_cell_method = iris.coords.CellMethod(method='mean', coords='time', intervals='6 hour')
+cell_method_constraint = iris.Constraint(cube_func=lambda cube: desired_cell_method in cube.cell_methods)
+
+for file in tqdm(glob.glob('*.pp')):
+    iris.save(iris.load(file, stash_constraint & cell_method_constraint), file[:-3]+'.nc')
+
