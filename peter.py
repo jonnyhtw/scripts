ce7b0cd7b3667d0b57e8ac58bd2633beb81f5d8a
Jonny Williams <jonny.williams@niwa.co.nz>
2023-09-07 15:18:38 +1200

    script for data for peter

--- peter.py
+++ peter.py
@@ -10,6 +10,7 @@ stash_constraint  =iris.AttributeConstraint(STASH='m01s03i236')
 desired_cell_method = iris.coords.CellMethod(method='mean', coords='time', intervals='6 hour')
 cell_method_constraint = iris.Constraint(cube_func=lambda cube: desired_cell_method in cube.cell_methods)
 
-for file in tqdm(glob.glob('*.pp')):
+for file in tqdm(sorted(glob.glob('*.pp'))):
+    print(file)
     iris.save(iris.load(file, stash_constraint & cell_method_constraint), file[:-3]+'.nc')
 
