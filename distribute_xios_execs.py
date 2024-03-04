c1adaa8e0f8ee53e9cc1b1884f3b9c0821d37b23
Jonny Williams <jonny.williams@niwa.co.nz>
2024-03-04 21:03:32 +1300

    files for distributing xios execs in lfric

---
+++ distribute_xios_execs.py
@@ -0,0 +1,13 @@
+n = 384
+nxios = 32
+
+f = open("distribute_xios_execs.txt", "w")
+
+for i in range(nxios):
+    f.write(str(2*i) + " /home/williamsjh/cylc-run/u-db797/run14/share/fcm_make_lfric/build/bin/lfric_atm configuration.nml\n")
+    f.write(str(2*i + 1) + " xios_server.exe\n")
+
+for i in range(2*nxios,n + nxios):
+    f.write(str(i) + " /home/williamsjh/cylc-run/u-db797/run14/share/fcm_make_lfric/build/bin/lfric_atm configuration.nml\n")
+
+f.close()
