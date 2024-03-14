b58ac746764dbd3523415aa1f54238fe0685a30d
Jonny Williams <jonny.williams@niwa.co.nz>
2024-03-14 00:56:55 +0000

    inputrc for ctrl f options

--- distribute_xios_execs.py
+++ distribute_xios_execs.py
@@ -1,15 +1,29 @@
+import sys
+
 n = 384
+ppn = 40
 nxios = 32
+nxios_on_each_node = 4
+
+if nxios > n:
+
+    print('nxios must be less than n, mpmd file not produced!')
+
+    sys.exit()
+
+else:
+
+    pass
 
 f = open("distribute_xios_execs.txt", "w")
 
-for j in range(8):
+for i in range(int(nxios/nxios_on_each_node)):
 
-    f.write(str(40*j) + '-' + str(40*j+3) + " /opt/nesi/XC50_sles15_skl/XIOS/r2279-CrayIntel-23.02-19-NC4PAR-patched/bin/xios_server.exe\n")
+    f.write(str(ppn*i) + '-' + str(ppn*i+(nxios_on_each_node-1)) + " /opt/nesi/XC50_sles15_skl/XIOS/r2279-CrayIntel-23.02-19-NC4PAR-patched/bin/xios_server.exe\n")
 
-    f.write(str(3+40*j+1)+'-'+str(3+40*j+36) + " /home/williamsjh/cylc-run/u-db797/run14/share/fcm_make_lfric/build/bin/lfric_atm configuration.nml\n")
+    f.write(str((nxios_on_each_node -1 )+ppn*i+1)+'-'+str((nxios_on_each_node-1)+ppn*i+(ppn - nxios_on_each_node)) + " /home/williamsjh/cylc-run/u-db797/run16/share/fcm_make_lfric/build/bin/lfric_atm configuration.nml\n")
 
-f.write(str(8*40) + "-"+str(8*40 +  n-8*36-1) + " /home/williamsjh/cylc-run/u-db797/run14/share/fcm_make_lfric/build/bin/lfric_atm configuration.nml\n")
+f.write(str((nxios/nxios_on_each_node)*ppn) + "-"+str((nxios/nxios_on_each_node)*ppn +  n-(nxios/nxios_on_each_node)*(ppn - nxios_on_each_node)-1) + " /home/williamsjh/cylc-run/u-db797/run16/share/fcm_make_lfric/build/bin/lfric_atm configuration.nml\n")
 
 
 f.close()
